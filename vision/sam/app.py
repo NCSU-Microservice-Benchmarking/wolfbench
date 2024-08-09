e1ca8e6dff0598eaf1d2d9cae7794d2e16f9085c
ubuntu <pwang25@ncsu.edu>
2024-08-09 23:57:12 +0000

    sam update

--- vision/sam/app.py
+++ vision/sam/app.py
@@ -1,6 +1,7 @@
-from ultralytics import SAM
+from traceback import print_tb
+
+import onnxruntime as ort
 from flask import Flask, request, Response
-import torch
 import cv2
 import numpy as np
 import requests
@@ -52,7 +53,9 @@ metrics.set_meter_provider(meter_provider)
 tracer = trace.get_tracer(__name__)
 
 # Load a model
-model = SAM('sam_b.pt')
+# model = SAM('sam_b.pt')
+# Initialize the YOLOv8 model
+ort_session = ort.InferenceSession('yolov8n.onnx')
 
 # Initialize the model
 # model = torch.hub.load('yolov5', 'custom', path='yolov5s.pt', source='local')
@@ -63,10 +66,122 @@ model = SAM('sam_b.pt')
 def greet():
     return("Hello")
 
+import onnxruntime as ort
+from flask import Flask, request, Response
+import cv2
+import numpy as np
 
-@app.route('/count_gpu')
-def countGPU():
-    return str(torch.cuda.device_count())
+app = Flask(__name__)
+
+# Load the YOLOv8 segmentation model
+ort_session = ort.InferenceSession('yolov8n-seg.onnx')
+
+@app.route('/segment_onnx', methods=['POST'])
+def segment_onnx():
+    # Retrieve image from the request
+    image_file = request.files.get('image')
+
+    if not image_file:
+        print("No image file received.")
+        return Response("No image file provided", status=400)
+
+    print(f"Received image: {image_file.filename}")
+    print(f"Image content type: {image_file.content_type}")
+    image_size = len(image_file.read())
+    print(f"Image size: {image_size} bytes")
+    image_file.seek(0)  # Reset file pointer after reading
+
+    # Decode the image and convert to float32
+    image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
+    print("Original image shape:", image.shape)
+
+    # Preprocess the image
+    image = preprocess_image(image)
+
+    # Prepare input for the ONNX model
+    input_name = ort_session.get_inputs()[0].name
+    output_name_list = [output.name for output in ort_session.get_outputs()]
+    print(f"Input blob shape: {image.shape}")
+    print(f"Model input names: {input_name}")
+
+    try:
+        # Perform inference
+        outputs = ort_session.run(output_name_list, {input_name: image})
+        print("ONNX model inference completed successfully.")
+    except Exception as e:
+        print(f"ONNX model inference failed: {e}")
+        return Response(f"ONNX model inference failed: {e}", status=500)
+
+    # Process and post-process the output
+
+    class_mask_dict = postprocess_output(outputs)
+    # Apply masks to the original image
+    segmented_image = apply_masks_to_image(image, class_mask_dict)
+    segmented_image = segmented_image.astype(np.uint8)
+    # Encode the image to PNG and return as response
+    segmented_image = segmented_image.transpose((2, 0, 1))
+    print("segmented image shape:", segmented_image.shape)
+
+    image_data = cv2.imencode('.png', segmented_image)[1].tobytes()
+    return Response(image_data, mimetype='image/png')
+
+
+def preprocess_image(image):
+    """Preprocess the input image to the format required by the ONNX model."""
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.astype(np.float32)
+    image = cv2.resize(image, (640, 640))  # Resize to the model's input size
+    image = image / 255.0
+    image = image.transpose((2, 0, 1))  # Change shape to (C, H, W)
+    image = np.expand_dims(image, axis=0)  # Add batch dimension (1, C, H, W)
+    return image
+
+
+def postprocess_output(outputs):
+     # do inference
+    model_input_size = (640,640)
+    # get the output_0 and output_1
+    output_0 = outputs[0] # 1 x 56 x 336: 336 predicted masks with (xc, yc, w, h, cls_conf1, cls_conf2, ..., cls_conf20, mask_weight1, mask_weight2, ..., mask_weight32)
+    output_1 = outputs[1] # 1 x 32 x 80 x 80: 32 masks in shape (80, 80) # NOTE: this is because YOLOv8 model outputs a 80x80 mask. Need to be configured if the model changes
+    output_0 = np.array(output_0, copy=True)
+    output_1 = np.array(output_1, copy=True)
+    # get the masks and weights by transpose from 1x56x336 to 56x336 and 1x32x32x32 to 32x32x32
+    output_class_and_weights = np.transpose(output_0[0], (1, 0))
+    output_masks = np.transpose(output_1[0], (1, 2, 0))
+    crop_detection_confidence_threshold = 0.5
+    # convert output class and weights to class, score, xc, yc, w, h, [mw1, mw2, ..., mw32]
+    output_class_and_weights = convertOutputClassAndWeights(output_class_and_weights, crop_detection_confidence_threshold)
+    # apply NMS to the output class and weights
+    output_class_and_weights = applyNMSForCropMonitoring(output_class_and_weights, crop_detection_confidence_threshold)
+    # split the output class and weights by class
+    class_mask_dict, image_avg_entropy = combineClassWeightsAndMasks(output_class_and_weights, output_masks, crop_detection_confidence_threshold, model_input_size[0], model_input_size[1])
+    print("class_mask_dict", class_mask_dict)
+    return  class_mask_dict
+
+
+def apply_masks_to_image(image, class_mask_dict):
+    """Apply the segmentation masks to the original image."""
+    image = np.squeeze(image)
+    print("Original image shape:", image.shape)
+
+    # 图像的形状应为 (C, H, W)
+    image_c, image_h, image_w = image.shape
+
+    for class_id, mask in class_mask_dict.items():
+        # Resize mask to match the height and width of the original image
+        resized_mask = cv2.resize(mask, (image_w, image_h))
+        binary_mask = (resized_mask > 0.5).astype(np.uint8)  # Convert to binary mask
+        binary_mask = np.expand_dims(binary_mask, axis=0)  # Add channel dimension, making it (1, H, W)
+
+        print("Image shape:", image.shape)
+        print("Mask shape:", binary_mask.shape)
+
+        # Apply the mask to each channel of the image
+        for c in range(image_c):
+            image[c] = image[c] * binary_mask
+
+    # 如果需要将图像转换回 `HWC` 格式，可以使用 `image = np.transpose(image, (1, 2, 0))`
+    return image
 
 @app.route('/',methods=['POST'])
 def segment():
@@ -109,6 +224,99 @@ def segment():
 
         # send the image to the client as an png image encoded as a binary string
         return Response(image, mimetype='image/png', headers=headers)
+
+def convertOutputClassAndWeights(
+    output_class_and_weights: np.ndarray,
+    crop_detection_confidence_threshold: float = 0.5,
+) -> np.ndarray:
+    csxywhmws_list = []
+    num_rows, num_cols = output_class_and_weights.shape
+    for row in range(num_rows):
+        x, y, w, h = output_class_and_weights[row, 0:4]
+        class_scores = output_class_and_weights[row, 4:4+80]
+        mask_weights = output_class_and_weights[row, 4+80:]
+        # get the max score
+        max_score = class_scores.max()
+        if max_score < crop_detection_confidence_threshold:
+            continue
+        # get the class index
+        class_idx = np.argmax(class_scores)
+        csxywhmws_list.append([class_idx, max_score, x, y, w, h] + mask_weights.tolist())
+    return np.array(csxywhmws_list)
+
+def combineClassWeightsAndMasks(class_and_weights: list, output_masks: np.ndarray, crop_detection_mask_threshold: float = 0.5, image_tile_width: float = 320, image_tile_height: float = 320) -> np.ndarray:
+    # transpose output_masks from 80 x 80 x 32 to 32 x 80 x 80
+    output_masks = np.transpose(output_masks, (2, 0, 1))
+    # sum up all the weights for the same class
+    class_mask_weight_dict = {} # class index -> mask weights
+    for i in range(len(class_and_weights)):
+        class_idx = int(class_and_weights[i][0])
+        mask_weight = class_and_weights[i][6:]
+        # broad cast the mask weight from N to N x 1 x 1
+        mask_weight = mask_weight.reshape(-1, 1, 1)
+        if class_idx not in class_mask_weight_dict:
+            class_mask_weight_dict[class_idx] = mask_weight
+        class_mask_weight_dict[class_idx] += mask_weight
+    # check if there exists valid class mask weight dict
+    if len(class_mask_weight_dict) == 0:
+        return {}, None
+    # get the masks for each class
+    dict_mask_idx_class_idx = {} # index in the composite mask -> class index
+    for mask_idx, class_idx in enumerate(class_mask_weight_dict.keys()):
+        dict_mask_idx_class_idx[mask_idx] = class_idx
+    masks = []
+    for class_idx, mask_weight in class_mask_weight_dict.items():
+        # mask_weight's shape is 32
+        # output_masks' shape is 32 x 80 x 80
+        # class mask is the sum of output_mask[i] * every value in mask_weight[i] for i in range(len(mask_weight))
+        class_mask = np.sum(output_masks * mask_weight, axis=0)
+        masks.append(class_mask)
+    # check if there exists valid masks
+    if len(masks) == 0:
+        return {}, None
+    # stack the masks
+    masks = np.stack(masks)
+    # apply sigmoid to the masks
+    masks = 1 / (1 + np.exp(-masks))
+    # from masks calculate the entropy as - masks[i, j, k] * log(masks[i, j, k])
+    entropy = -np.sum(masks * np.log(masks + 1e-6)) / masks.size
+    # filter out the masks with the mask threshold
+    masks = (masks > crop_detection_mask_threshold) * 1.0
+    # resize the masks from N x 80 x 80 to N x image_tile_height x image_tile_width
+    masks = np.array([cv2.resize(mask, (image_tile_width, image_tile_height)).T for mask in masks])
+    # distribute the masks to the class_mask_dict
+    class_mask_dict = {} # class index -> mask
+    for mask_idx in range(len(masks)):
+        class_idx = dict_mask_idx_class_idx[mask_idx]
+        class_mask_dict[class_idx] = masks[mask_idx]
+        # # # draw the mask
+        # cv2.imwrite(f"mask_{class_idx}.png", masks[mask_idx] * 255)
+    return class_mask_dict, entropy
+
+def applyNMSForCropMonitoring(
+    detection_result_list: np.ndarray,
+    crop_detection_confidence_threshold: float = 0.5,
+) -> np.ndarray:
+    # group detection results by class
+    class_dict = {} # class index -> list of detection results
+    for detection_result in detection_result_list:
+        class_idx = int(detection_result[0])
+        if class_idx not in class_dict:
+            class_dict[class_idx] = []
+        class_dict[class_idx].append(detection_result)
+    # apply NMS to each class
+    for class_idx in class_dict:
+        class_detection_result_list = np.array(class_dict[class_idx])
+        boxes = class_detection_result_list[:, 2:6]
+        scores = class_detection_result_list[:, 1]
+        class_result_indices = cv2.dnn.NMSBoxes(boxes, scores, crop_detection_confidence_threshold, 0.5)
+        # filter out the detection results
+        class_dict[class_idx] = class_detection_result_list[class_result_indices]
+    # merge the detection results
+    detection_result_list = []
+    for class_idx in class_dict:
+        detection_result_list.extend(class_dict[class_idx])
+    return detection_result_list
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001)
--- vision/yolov8/app.py
+++ vision/yolov8/app.py
@@ -56,9 +56,8 @@ def greet():
 @app.route('/count_gpu')
 def countGPU():
     return "0"
-
-@app.route('/traceless_detections', methods=['POST'])
-def traceless_detect():
+@app.route('/detections_onnx', methods=['POST'])
+def detections_onnx():
     # Retrieve image from the request
     image_file = request.files.get('image')
 
@@ -71,54 +70,73 @@ def traceless_detect():
     else:
         print("No image file received.")
         return Response("No image file provided", status=400)
+
+    # Decode the image and convert to float32
     image = cv2.imdecode(np.frombuffer(image_file.read(), np.uint8), cv2.IMREAD_COLOR)
-    # image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-    image = image.astype(np.float32)
     print("Original image shape:", image.shape)
 
-    # image = np.expand_dims(image, axis=0)
+    # Convert the image to the required format
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = image.astype(np.float32)
 
-    input_blob = cv2.dnn.blobFromImage(image, scalefactor=1 / 255.0, size=(640, 640), mean=(0, 0, 0),
-                                       swapRB=True, crop=False)
+    # Resize the image to the required input size of the model
+    model_input_size = (640, 640)  # This should match the input size expected by your ONNX model
+    image = cv2.resize(image, model_input_size)
 
-    print(f"Input blob shape: {input_blob.shape}")
-    ort_inputs = {ort_session.get_inputs()[0].name: input_blob}
-    print(f"Model input names: {ort_session.get_inputs()[0].name}")
+    # Normalize the image
+    image = image / 255.0
+    image = image.transpose((2, 0, 1))  # Change the shape to (C, H, W)
+    image = np.expand_dims(image, axis=0)  # Add batch dimension, making the shape (1, C, H, W)
+
+    # Prepare the input for ONNX model
+    input_name = ort_session.get_inputs()[0].name
+    output_name_list = [output.name for output in ort_session.get_outputs()]
+    print(f"Input blob shape: {image.shape}")
+    print(f"Model input names: {input_name}")
 
     try:
-        ort_outs = ort_session.run(None, ort_inputs)
+        # Perform inference
+        outputs = ort_session.run(output_name_list, {input_name: image})
         print("ONNX model inference completed successfully.")
     except Exception as e:
         print(f"ONNX model inference failed: {e}")
         return Response(f"ONNX model inference failed: {e}", status=500)
 
-    outputs = ort_outs[0]  # 获取 ONNX 模型的输出
-    print(f"Model output shape: {outputs.shape}")
-    print(f"Model output sample (first detection): {outputs[0]}")
+    # Process the output from ONNX model
+    output = outputs[0]
+    output = np.array(output, copy=True)
+    print(f"Model output shape: {output.shape}")
+    print(f"Model output sample (first detection): {output[0]}")
 
-    results = []
+    detection_raw_result_list = output.squeeze()
+    detection_raw_result_list = np.transpose(detection_raw_result_list, (1, 0))
 
-    confidence_threshold = 0.5
+    # Convert raw result to result list in (class, score, x, y, w, h) format
+    detection_result_list, average_raw_entropy = detectionRawListToCSXYWH(detection_raw_result_list,
+                                                                          object_detection_confidence_threshold=0.5)
 
-    confidence_threshold = 0.5
-    for detection in outputs[0]:  # 遍历每个检测框
-        x, y, w, h = detection[0:4]
-        confidence = detection[4]
-        if confidence > confidence_threshold:
-            class_probs = detection[5:]
-            class_id = np.argmax(class_probs)
-            results.append((x, y, w, h, confidence, class_id))
+    # Apply NMS to the detection results
+    detection_result_list = applyNMSForObjectDetection(detection_result_list, object_detection_confidence_threshold=0.5)
 
-    # Note: Modify as per your actual plotting logic
-    for result in results:
-        x, y, w, h, confidence, class_id = result
-        image = cv2.rectangle(image, (int(x), int(y)), (int(x + w), int(y + h)), (0, 255, 0), 2)
-        image = cv2.putText(image, f'Class: {class_id}, Conf: {confidence:.2f}', (int(x), int(y) - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    if image.shape[0] == 1:
+        image = np.squeeze(image, axis=0)
+    print("Image shape after processing:", image.shape)
+    if image.shape == (3, 640, 640):
+        image = image.transpose(1, 2, 0)
 
+    if image.dtype != np.uint8:
+        image = (image * 255).astype(np.uint8)
+
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+
+    for result in detection_result_list:
+        class_idx, class_score, x, y, w, h = result
+        image = cv2.rectangle(image, (int(x-w/2), int(y-h/2)), (int(x + w/2), int(y + h/2)), (0, 255, 0), 2)
+
+        # image = cv2.putText(image, f'Class: {str(class_idx)}, Conf: {class_score:.2f}',
+        #                     (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
     # Encode the image to PNG
     image_data = cv2.imencode('.png', image)[1].tobytes()
-
     return Response(image_data, mimetype='image/png')
 
 
@@ -183,6 +201,72 @@ def detect():
             TraceContextTextMapPropagator().inject(response_headers, context)
 
         return Response(image_data, mimetype='image/png', headers=response_headers)
+
+def convertOutputClassAndWeights(
+    output_class_and_weights: np.ndarray,
+    crop_detection_confidence_threshold: float = 0.5,
+) -> np.ndarray:
+    csxywhmws_list = []
+    num_rows, num_cols = output_class_and_weights.shape
+    for row in range(num_rows):
+        x, y, w, h = output_class_and_weights[row, 0:4]
+        class_scores = output_class_and_weights[row, 4:4+20]
+        mask_weights = output_class_and_weights[row, 4+20:]
+        # get the max score
+        max_score = class_scores.max()
+        if max_score < crop_detection_confidence_threshold:
+            continue
+        # get the class index
+        class_idx = np.argmax(class_scores)
+        csxywhmws_list.append([class_idx, max_score, x, y, w, h] + mask_weights.tolist())
+    return np.array(csxywhmws_list)
+
+
+def detectionRawListToCSXYWH(detection_raw_results: np.ndarray, object_detection_confidence_threshold: float) -> np.ndarray:
+    csxywh_list = []
+    entropy_list = []
+    if detection_raw_results.size == 0:
+        return np.array(csxywh_list), -1 # return -1 if no detection results
+    num_rows, num_cols = detection_raw_results.shape
+    for row in range(num_rows):
+        x, y, w, h = detection_raw_results[row, 0:4]
+        class_scores = detection_raw_results[row, 4:]
+        # compute the entropy of the class scores
+        entropy = - np.sum(class_scores * np.log(class_scores + 1e-6)) / len(class_scores)
+        entropy_list.append(entropy)
+        # get the max score
+        max_score = class_scores.max()
+        if max_score < object_detection_confidence_threshold:
+            continue
+        # get the class index
+        class_idx = np.argmax(class_scores)
+        csxywh_list.append([class_idx, max_score, x, y, w, h])
+    average_raw_entropy = np.mean(entropy_list) if len(entropy_list) > 0 else -1
+    return np.array(csxywh_list), average_raw_entropy
+
+def applyNMSForObjectDetection(detection_result_list: np.ndarray, object_detection_confidence_threshold: float) -> np.ndarray:
+    # group detection results by class
+    class_dict = {} # class index -> list of detection results
+    for detection_result in detection_result_list:
+        class_idx = int(detection_result[0])
+        if class_idx not in class_dict:
+            class_dict[class_idx] = []
+        class_dict[class_idx].append(detection_result)
+    # apply NMS to each class
+    if len(class_dict) == 0: # no detection results, return empty list
+        return []
+    for class_idx in class_dict:
+        class_detection_result_list = np.array(class_dict[class_idx])
+        boxes = class_detection_result_list[:, 2:]
+        scores = class_detection_result_list[:, 1]
+        class_result_indices = cv2.dnn.NMSBoxes(boxes, scores, object_detection_confidence_threshold, 0.5)
+        class_dict[class_idx] = class_detection_result_list[class_result_indices]
+    # merge the detection results
+    detection_result_list = []
+    for class_idx in class_dict:
+        detection_result_list.extend(class_dict[class_idx])
+    return detection_result_list
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
